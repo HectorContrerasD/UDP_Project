ce61de5a9d9329896a485cf337ea5c0c6a6ddf50
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2025-02-28 23:45:13 -0600

    Service and basic folders added

---
+++ Client/Models/DTOs/AnswerDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models.DTOs
+{
+    public class AnswerDTO
+    {
+        public string? Name { get; set; }
+        public string? Answer { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ Client/Services/ClientService.cs
@@ -0,0 +1,26 @@
+﻿using Client.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Client.Services
+{
+    public class ClientService
+    {
+        private UdpClient client = new();
+        public string Server { get; set; } = "0.0.0.0";
+        public void SendAnswer(AnswerDTO answerDTO)
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Parse(Server), 5001);
+            answerDTO.Name = Dns.GetHostName();
+            var json = JsonSerializer.Serialize(answerDTO);
+            byte[] buffer= Encoding.UTF8.GetBytes(json);
+            client.Send(buffer,buffer.Length, ipEndPoint);
+        }
+    }
+}
