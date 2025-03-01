a69103b59e2dd80a6be3d8b64c8a5ec7dd1907a9
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2025-02-28 23:29:33 -0600

    Server services and models added

---
+++ Server/Models/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models.DTOs
+{
+    public class UserDTO
+    {
+        public string? Name { get; set; }
+        public string? Answer { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ Server/Services/ServerService.cs
@@ -0,0 +1,44 @@
+﻿using Server.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Server.Services
+{
+    public class ServerService
+    {
+        public ServerService()
+        {
+            var thread = new Thread(new ThreadStart(Start))
+            {
+                IsBackground = true
+            };
+            thread.Start();
+        }
+        public event EventHandler<UserDTO>? ValidateAnswer;
+        void Start()
+        {
+            UdpClient server = new(5001);
+            while (true)
+            {
+                IPEndPoint remote = new(IPAddress.Any, 5001);
+                byte[] buffer = server.Receive(ref remote);
+                UserDTO? user = JsonSerializer.Deserialize<UserDTO>(Encoding.UTF8.GetString(buffer));
+            
+                if (user != null)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        ValidateAnswer?.Invoke(this, user);
+                    });
+                }
+            }
+        }
+    }
+}
